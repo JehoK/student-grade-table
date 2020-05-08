c6134c5af9bba9a0501b9229e5298119e2267d02
Jaeho Kim <jehokim90@gmail.com>
2020-05-07 20:49:31 -0700

    add-grade

--- components/app.js
+++ components/app.js
@@ -1,10 +1,14 @@
 
 class App {
-  constructor(gradeTable, pageHeader){
+  constructor(gradeTable, pageHeader, gradeForm){
     this.handleGetGradeError = this.handleGetGradeError.bind(this);
     this.handleGetGradeSuccess = this.handleGetGradeSuccess.bind(this);
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
+    this.gradeForm = gradeForm;
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   }
 
   handleGetGradeError(error) {
@@ -16,8 +20,14 @@ class App {
     for (let i =0; i<grades.length; i++){
       total += grades[i].grade;
     }
-    var average = (total / grades.length)
+    var average = Math.floor(total / grades.length);
     this.pageHeader.updateAverage(average);
+  }
+  handleCreateGradeError(error) {
+    console.error(error);
+  }
+  handleCreateGradeSuccess() {
+    this.getGrade();
   }
   getGrade(){
     $.ajax({
@@ -30,5 +40,22 @@ class App {
   }
   start(){
     this.getGrade();
+    this.gradeForm.onsubmit(this.createGrade);
+  }
+
+  createGrade(name, course, grade){
+    console.log(name, course, grade)
+    $.ajax({
+      method: "POST",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: { "X-Access-Token": "2aoZsbBj" },
+      data: {
+        "name": name,
+        "course": course,
+        "grade":  grade
+      },
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError
+    })
   }
 }
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var table = document.querySelector("table");
 var gradeTable = new GradeTable(table);
-
+var formElement = document.querySelector("form");
 
 var myHeader =document.querySelector("header");
 var newAverage = new PageHeader(myHeader);
-var app = new App(gradeTable, newAverage);
+var gradeForm = new GradeForm(formElement);
+var app = new App(gradeTable, newAverage, gradeForm);
 
 app.start();

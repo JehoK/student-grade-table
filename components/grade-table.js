b462b36e6a2a36936b0c3e49bcb2efe79790f62d
Jaeho Kim <jehokim90@gmail.com>
2020-06-16 16:20:13 -0700

    view-all-grades

---
+++ components/grade-table.js
@@ -0,0 +1,20 @@
+class GradeTable {
+  constructor(tableElement){
+    this.tableElement = tableElement;
+  }
+  updateGrades(grades){
+    console.log(grades);
+    var tbody = document.querySelector("tbody");
+    for(var i=0; i<grades.length; i++){
+      var tbodyRow = document.createElement("tr");
+      var student = document.createElement("td");
+      var course = document.createElement("td");
+      var studentGrades = document.createElement("td");
+      student.textContent = grades[i].name;
+      course.textContent = grades[i].course;
+      studentGrades.textContent = grades[i].grade;
+      tbodyRow.append(student, course, studentGrades);
+      tbody.append(tbodyRow);
+    }
+  }
+}
